439272ab514bcccef6fe4cfcb48ba67ef3f1897f
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-03-18 20:00:23 +0100

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ninshubur
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/ninshubur/core_test.clj
@@ -0,0 +1,7 @@
+(ns ninshubur.core-test
+  (:use clojure.test
+        ninshubur.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
