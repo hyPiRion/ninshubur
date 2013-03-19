83b7b663375b699461cacd8e069db6b7fb276f4a
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-03-19 12:44:54 +0100

    Naive connection to Genetica.

--- src/ninshubur/core.clj
+++ src/ninshubur/core.clj
@@ -1,7 +1,18 @@
 (ns ninshubur.core
+  (:require [clojure.java.io :as io]
+            [clojure.pprint :as pp :refer [cl-format]])
+  (:import (java.net Socket))
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Basic connector to Genetica."
   [& args]
-  (println "Hello, World!"))
+  (let [s (Socket. "localhost" 1055)]
+    (try
+      (with-open [in (io/reader s)
+                  out (io/writer s)]
+        (cl-format out "[100, foobar].~%")
+        (doseq [line (line-seq in)]
+          (println (read-string line))))
+      (finally
+        (.close s)))))
