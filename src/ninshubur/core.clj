b360d16727d25311158bc36fee7e0c7d32b60c41
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-03-19 13:54:47 +0100

    Call one_max hardcoded.

--- src/ninshubur/core.clj
+++ src/ninshubur/core.clj
@@ -4,15 +4,23 @@
   (:import (java.net Socket))
   (:gen-class))
 
+(defn fmt [out string & args]
+  (cl-format *out* (str "out=> " string) args)
+  (cl-format out string args))
+
 (defn -main
   "Basic connector to Genetica."
   [& args]
-  (let [s (Socket. "localhost" 1055)]
+  (let [s (Socket. "localhost" 1055)
+        lcount (atom 0)]
     (try
       (with-open [in (io/reader s)
                   out (io/writer s)]
-        (cl-format out "[100, foobar].~%")
+        (fmt out (str "[100, 120, roulette, 60, 0.3, fitness, "
+                      "full_replacement, 10, one_max, 40, 0.2, 0.001, "
+                      "1, 0.7].~%"))
         (doseq [line (line-seq in)]
-          (println (read-string line))))
+          (swap! lcount inc)
+          (println @lcount "=>" (read-string line))))
       (finally
         (.close s)))))
