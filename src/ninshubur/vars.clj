897097a16defc2962e078e7c0089aa123b806994
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-03-21 22:51:14 +0100

    Add vars.

---
+++ src/ninshubur/vars.clj
@@ -0,0 +1,7 @@
+(ns ninshubur.vars)
+
+(def ^:dynamic *area-width* 30)
+(def ^:dynamic *area-height* 15)
+(def ^:dynamic *tracker-size* 5)
+(def ^:dynamic *nof-blocks* 40)
+(def ^:dynamic *block-max-size* 6)
