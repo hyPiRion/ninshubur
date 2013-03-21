13e56fdab16e8b6e7abff88390427b21ba1ed979
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-03-21 22:50:25 +0100

    Update CLUI and simulator.

--- src/ninshubur/simulate.clj
+++ src/ninshubur/simulate.clj
@@ -1,8 +1,32 @@
 (ns ninshubur.simulate
-  (:require [ninshubur.term :as t]
-            [lanterna.terminal :as tty]))
+  (:require [ninshubur.term :as t :refer [*t*]]
+            [lanterna.terminal :as tty]
+            [ninshubur.vars :as v]))
 
-(defn start-term []
-  (t/in-term
-   (t/draw-simulation #{{:char \x :color :red :y 3 :x 5 :length 6}})
-   (tty/get-key-blocking t/term)))
+(defn make-block []
+  (let [len (inc (rand-int v/*block-max-size*))
+        pos (rand-int v/*area-width*)
+        catch? (< len v/*tracker-size*)]
+    {:length len, :x pos, :y 0,
+     :color (if catch? :green :red), :char (if catch? \o \O)}))
+
+(defn init-tracker []
+  {:length v/*tracker-size*, :y (dec v/*area-height*)
+   :x 0, :color :blue, :char \-})
+
+(defn init-state [neuron-cluster]
+  (merge neuron-cluster
+         {:time 0
+          :hist [{:tracker (init-tracker)
+                  :block (make-block)
+                  :a {:o 0 :y 0}
+                  :b {:o 0 :y 0}
+                  :c {:o 0 :y 0}
+                  :d {:o 0 :y 0}}]}))
+
+(defn start-term [state]
+  (let [time (:time state)
+        block (get-in state [:hist time :block])
+        tracker (get-in state [:hist time :tracker])]
+    (t/draw-simulation #{block tracker})
+    (while (not= \q (t/get-key)))))
--- src/ninshubur/term.clj
+++ src/ninshubur/term.clj
@@ -1,37 +1,40 @@
 (ns ninshubur.term
-  (:require [lanterna.terminal :as t]))
+  (:require [lanterna.terminal :as t]
+            [ninshubur.vars :as v]))
 
 (def ^:dynamic *t*)
-
-(def area-height 15)
-(def area-width 20)
 
 (defn draw-border []
   (t/set-fg-color *t* :white)
   (t/set-bg-color *t* :black)
   (t/move-cursor *t* 0 0)
-  (dotimes [x (+ area-width 2)]
+  (dotimes [x (+ v/*area-width* 2)]
     (t/put-character *t* \#))
-  (dotimes [y area-height]
+  (dotimes [y v/*area-height*]
     (t/move-cursor *t* 0 (inc y))
     (t/put-character *t* \#)
-    (t/move-cursor *t* (inc area-width) (inc y))
+    (t/move-cursor *t* (inc v/*area-width*) (inc y))
     (t/put-character *t* \#))
-  (t/move-cursor *t* 0 (+ 1 area-height))
-  (dotimes [x (+ area-width 2)]
+  (t/move-cursor *t* 0 (+ 1 v/*area-height*))
+  (dotimes [x (+ v/*area-width* 2)]
     (t/put-character *t* \#)))
 
 (defn draw-object [object]
   (let [{:keys [color char y x length]} object]
     (t/set-fg-color *t* color)
-    (t/move-cursor *t* x y)
+    (t/move-cursor *t* (inc x) (inc y))
     (dotimes [i length]
       (t/put-character *t* char))))
 
 (defn draw-simulation [objects]
+  (t/clear *t*)
   (draw-border)
   (doseq [obj objects]
     (draw-object obj)))
 
+(defn get-key []
+  (t/get-key-blocking *t*))
+
 (defmacro in-term [& body]
-  `(t/in-terminal *t* ~@body))
+  `(binding [*t* (t/get-terminal :text)]
+     (t/in-terminal *t* ~@body)))
